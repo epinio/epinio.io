a269487c73d1f3c77c3b746e74b92b42c210106e
David Johnson <dcharles525@gmail.com>
2026-07-02 13:22:53 -0400

    feat(Updating branch with latest version of site):

--- src/assets/data/nav-items.js
+++ src/assets/data/nav-items.js
@@ -1,28 +1,43 @@
 export const navItems = [
   {
     title: "How It Works",
-    url: "/how-it-works",
+    url: "/epinio-web-astro/how-it-works",
     external: false,
   },
   {
-    title: "Helm Charts",
-    url: "https://artifacthub.io/packages/helm/epinio/epinio",
+    title: "FAQ",
+    url: "/epinio-web-astro/faq",
+    external: false,
+  },
+  {
+    title: "Blog",
+    url: "http://docs.epinio.io/blog",
     external: true,
   },
   {
-    title: "Slack",
-    url: "https://rancher-users.slack.com/?redir=%2Fmessages%2Fepinio",
-    external: true,
-  },
-  {
-    title: "Documentation",
-    url: "https://docs.epinio.io",
-    external: true,
-  },
-  {
-    title: "GitHub",
-    url: "https://github.com/epinio/epinio/",
-    external: true,
+    title: "Resources",
+    children: [
+      {
+        title: "Helm Charts",
+        url: "https://artifacthub.io/packages/helm/epinio/epinio",
+        external: true,
+      },
+      {
+        title: "Slack",
+        url: "https://rancher-users.slack.com/?redir=%2Fmessages%2Fepinio",
+        external: true,
+      },
+      {
+        title: "Documentation",
+        url: "https://docs.epinio.io",
+        external: true,
+      },
+      {
+        title: "GitHub",
+        url: "https://github.com/epinio/epinio/",
+        external: true,
+      },
+    ],
   },
 ];
 
